6b7dcdb06b4a57cbfa55b7ef306de423bad84b4d
Shaunak G <sgadkari.sg@gmail.com>
2016-04-26 16:38:20 +1000

    Added files via upload

---
+++ main.bat
@@ -0,0 +1,18 @@
+title Remote Connection GUI by BoneCorp
+echo off
+cls
+:main
+echo Choose RDP Client:
+echo 1. Jaytra
+echo 2. Aidan
+echo 3. Levi
+echo 4. Vicente
+echo 5. Ishaan
+choice /c 12345 /m Choose:
+if %errorlevel%==1 start rdps\jay.RDP
+if %errorlevel%==2 start rdps\aidan.RDP
+if %errorlevel%==3 start rdps\levi.RDP
+if %errorlevel%==4 start rdps\vicente.RDP
+if %errorlevel%==5 start rdps\ishaan.RDP
+exit
+REM
